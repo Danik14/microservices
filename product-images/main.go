611178f41c9d4ab1e1628bd3612e017a85c30b86
Danik <chapagana@gmail.com>
2022-12-01 14:43:45 +0600

    Added graceful shutdown

--- product-images/main.go
+++ product-images/main.go
@@ -1,6 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Danik14/microservices/product-images/handlers"
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/go-hclog"
@@ -18,7 +27,7 @@ func main() {
 		},
 	)
 
-	// sl := l.StandardLogger(&hclog.StandardLoggerOptions{InferLevels: true})
+	sl := l.StandardLogger(&hclog.StandardLoggerOptions{InferLevels: true})
 
 	// create the storage class, use local storage
 	// max filesize 5MB
@@ -31,7 +40,44 @@ func main() {
 	f := handlers.NewFiles(l)
 
 	router.POST("/images/:id/:filename", f.SaveFile)
-	router.GET("/images/:filename", f.ServeFile)
-	router.Run(":8080")
+	router.GET("/images/:id/:filename", f.ServeFile)
+
+	srv := &http.Server{
+		Addr:     ":8080",
+		Handler:  router,
+		ErrorLog: sl,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	// Making buffered channel, if not giving size
+	// throwing warning, not sure if made perfectly
+	quit := make(chan os.Signal, 1)
+
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 
 }
